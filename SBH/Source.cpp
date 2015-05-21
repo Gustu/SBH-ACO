f9c5c0b60204d4c28381939058c0a1ef4b3deb0b
hya <aproszciebardzo@gmail.com>
2015-05-21 22:48:12 +0200

    Init

---
+++ SBH/Source.cpp
@@ -0,0 +1,16 @@
+#include <ogdf/basic/Graph.h>
+#include <ogdf/basic/graph_generators.h>
+#include <ogdf/layered/DfsAcyclicSubgraph.h>
+
+using namespace ogdf;
+
+int main()
+{
+	Graph G;
+	randomSimpleGraph(G, 10, 20);
+	DfsAcyclicSubgraph DAS;
+	DAS.callAndReverse(G);
+	G.writeGML("test.gml");
+
+	return 0;
+}
